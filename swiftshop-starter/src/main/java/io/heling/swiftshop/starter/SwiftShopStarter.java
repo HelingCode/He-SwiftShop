4a9d391c9a61fc86bb56979cb326e05e9dd019a6
HelingCode <929926540@qq.com>
2023-05-27 13:51:18 +0800

    feat: 项目初始化

---
+++ swiftshop-starter/src/main/java/io/heling/swiftshop/starter/SwiftShopStarter.java
@@ -0,0 +1,18 @@
+package io.heling.swiftshop.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heling
+ * @version 1.0.0
+ * @date 2023/5/27 13:48
+ * @description 项目启动类
+ */
+@SpringBootApplication
+public class SwiftShopStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwiftShopStarter.class,args);
+    }
+}
